e04bd9b139872b6b76830b8e897c488e4144ee13
niblit <niblit.gh@gmail.com>
2024-06-23 01:21:38 -0600

    Modified scramble input a little

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use rubik_solver::Cube;
 
+const SCRAMBLE: &str = "L2 D2 R U2 F2 L U2 L D2 R2 F2 R' D F D R D L' B L'";
+
 fn main() {
     let mut cube = Cube::default();
     println!("{cube}");
 
-    cube.scramble("L2 D2 R U2 F2 L U2 L D2 R2 F2 R' D F D R D L' B L'");
+    cube.scramble(SCRAMBLE);
     println!("{cube}");
-
 }
